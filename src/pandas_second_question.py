76040fb541a79709a0eb6b0c1bae081927b1262d
ArnoVel <arno.vel1996@gmail.com>
2021-05-12 19:41:27 +0200

    Added documentation for second question.

--- src/pandas_second_question.py
+++ src/pandas_second_question.py
@@ -2,6 +2,14 @@ import pandas as pd
 from datetime import datetime
 
 def compute_weekday(df):
+    """
+    Takes a pandas DataFrame and computes the weekday using the appropriate columns for each row.
+
+    Args:
+        df (pd.DataFrame): input DataFrame with datetimes
+    Returns:
+        pd.Series: column with the corresponding weekday as integers
+    """
     days = df['pickup_datetime'].apply(
         lambda datetime_as_str: datetime.strptime(datetime_as_str,'%Y-%m-%d %H:%M:%S').weekday()
         )
@@ -9,6 +17,14 @@ def compute_weekday(df):
     return days
 
 def compute_count_per_weekday(df):
+    """
+    Takes a pandas DataFrame and computes the number of taxi rides for each weekday.
+
+    Args:
+        df (pd.DataFrame): input DataFrame with datetimes
+    Returns:
+        pd.DataFrame: dataframe with the corresponding weekday and count as integers
+    """
     # find weekday for each row
     days = compute_weekday(df)
     df['day'] = days
